39d439f23c55b83879a66186094cfb761659957d
UnownDragon666 <unown.idrees@gmail.com>
2025-03-22 22:45:08 +1300

    Index complete

--- js_files/index_startScript.js
+++ js_files/index_startScript.js
@@ -3,9 +3,16 @@
 // Index script
 // Written by Idrees Munshi
 /*******************************************************/
+console.log('%cindex_startScrip.js', 'color: blue; background-color: white;');
 
-
+/*******************************************************/
+// index_toInstructionsPage()
+// Called when user clicks on start page
+// Moves to instructions page
+// Input: N/A
+// Returns: N/A
+/*******************************************************/
 function index_toInstructionsPage() {
-    console.log('gameScreen')
+    console.log('gameScreen');
     document.location.href = 'html_files/ins_gameInstructions.html';
 }
