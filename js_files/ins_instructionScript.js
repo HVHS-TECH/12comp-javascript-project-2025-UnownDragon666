a302eade887b545a92bd00177be5b43768e9ad70
UnownDragon666 <unown.idrees@gmail.com>
2025-03-23 01:07:25 +1300

    Working on game settings

--- js_files/ins_instructionScript.js
+++ js_files/ins_instructionScript.js
@@ -32,7 +32,7 @@ function ins_denyHelp() {
 // ins_displayInstructions()
 // Called by button in ins_gameInstructions.html
 // Replaces the <p> tags' innerHTML to display instructions
-// Remove buttons, create a button to go to gameSettings page
+// Calls funtion to remove buttons, create a button to go to gameSettings page
 // Input: N/A
 // Returns: N/A
 /*******************************************************/
@@ -44,5 +44,32 @@ function ins_displayInstructions() {
 
     // Display instructions in p_instructionText
     document.getElementById('p_instructionText').innerHTML = "Use left and right arrow keys, or A and D keys to move the collector. <br> Collect as many stars as you can without collecting the asteroids. <br>"
-    
+
+    // Call function to change buttons to the button that goes to the game settings page
+    ins_changeButtons();
 }   
+
+/*******************************************************/
+// ins_displayInstructions()
+// Called by ins_displayInstructions()
+// Remove buttons, create a button to go to gameSettings page
+// Input: N/A
+// Returns: N/A
+/*******************************************************/
+function ins_changeButtons() {
+    // Remove existing buttons
+    document.getElementById('b_denyButton').remove();
+    document.getElementById('b_acceptButton').remove();
+
+    // Create new button to go to next page
+    let ins_nextPageButton = document.createElement('button');
+    ins_nextPageButton.innerHTML = "Choose your difficulty";
+
+    // Add button functionality
+    ins_nextPageButton.onclick = () => {
+        window.location.assign('../html_files/set_gameSettingsScreen.html');
+    }
+
+    // Append the element to the DOM (So it becomes visible on the page)
+    document.getElementById('d_buttonContainer').appendChild(ins_nextPageButton);
+}
